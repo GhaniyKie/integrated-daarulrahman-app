c3805460d5200c64aed91f223caea2111ca68798
= <abdulhakimghaniy@outlook.co.id>
2021-04-19 14:01:00 +0700

    Buat validasi Password sesuai dengan regex

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -1,29 +1,37 @@
 class API::RegistrationsController < Devise::RegistrationsController
     skip_before_action :verify_authenticity_token
 
-    def create
-        user = User.new(email: params[:email], password: params[:password])
+    include API::APIHelper
 
-        if user.save
-            # iat (Issued At): Waktu dimana JWT ini di berikan ke pengguna
-            # jti (JWT ID): Id hasil dari payload iat dan personal token sebagai identifikasi Token JWT
-            iat = Time.now.to_i
-            jti = Digest::MD5.hexdigest("#{iat}:#{user.personal_token}")
-      
-            payload = {
-                id: user.id,
-                email: user.email,
-                jti: jti,
-                iat: iat        
-            }
-            encoded = API::JsonWebToken.encode(payload)
-            render  json: { 
-                    token: encoded,
+    def create
+        if password_param.match(PASSWORD_VALIDATION)
+            user = User.new(email: params[:email], password: params[:password])
+        
+            if user.save
+                # iat (Issued At): Waktu dimana JWT ini di berikan ke pengguna
+                # jti (JWT ID): Id hasil dari payload iat dan personal token sebagai identifikasi Token JWT
+                iat = Time.now.to_i
+                jti = Digest::MD5.hexdigest("#{iat}:#{user.personal_token}")
+        
+                payload = {
+                    id: user.id,
                     email: user.email,
-                    message: "Sesi login ini akan kadaluarsa pada pukul #{Time.at(payload[:exp]).strftime('%k:%M:%S')}"
-            },      status: 200
+                    jti: jti,
+                    iat: iat        
+                }
+                encoded = API::JsonWebToken.encode(payload)
+                render  json: { 
+                        token: encoded,
+                        email: user.email,
+                        message: "Sesi login ini akan kadaluarsa pada pukul #{Time.at(payload[:exp]).strftime('%k:%M:%S')}"
+                },      status: 200
+            else
+                render json: { error: user.errors.full_messages[0] }, status: 422 # Unprocessable Entity
+            end
         else
-            render json: user.errors.full_messages, status: 422 # Unprocessable Entity
+            render json: { 
+                error: "Password tidak valid. Pastikan password terdiri dari: Minimum 6 karakter, 1 kapital, 1 angka dan 1 karakter unik. Contoh: Gh4n!y" }, 
+                status: 422
         end
     end
 
